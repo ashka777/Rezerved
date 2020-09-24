e03a76f4cb917ce18b81c49131db34281cc9eea3
ashka777 <34421085+ashka777@users.noreply.github.com>
2020-09-24 15:28:03 +0300

    Add files via upload
    РЕЗЕРВНОЕ КОПИРОВАНИЕ fw 4.5

---
+++ WindowsFormsApplication6/Form1.Designer.cs
@@ -0,0 +1,374 @@
+﻿namespace WindowsFormsApplication6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button_Stop = new System.Windows.Forms.Button();
+            this.button_Start = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label_Action = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_From1 = new System.Windows.Forms.TextBox();
+            this.textBox_To = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_From2 = new System.Windows.Forms.TextBox();
+            this.textBox_From3 = new System.Windows.Forms.TextBox();
+            this.button_About = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.numericUpDown_timer = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_timer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_Stop
+            // 
+            this.button_Stop.Location = new System.Drawing.Point(28, 377);
+            this.button_Stop.Name = "button_Stop";
+            this.button_Stop.Size = new System.Drawing.Size(259, 46);
+            this.button_Stop.TabIndex = 0;
+            this.button_Stop.Text = "СТОП";
+            this.button_Stop.UseVisualStyleBackColor = true;
+            this.button_Stop.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button_Start
+            // 
+            this.button_Start.Location = new System.Drawing.Point(28, 305);
+            this.button_Start.Name = "button_Start";
+            this.button_Start.Size = new System.Drawing.Size(304, 41);
+            this.button_Start.TabIndex = 1;
+            this.button_Start.Text = "Начать копирование";
+            this.button_Start.UseVisualStyleBackColor = true;
+            this.button_Start.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label_Action
+            // 
+            this.label_Action.Location = new System.Drawing.Point(7, 349);
+            this.label_Action.Name = "label_Action";
+            this.label_Action.Size = new System.Drawing.Size(336, 23);
+            this.label_Action.TabIndex = 2;
+            this.label_Action.Text = "Действия / ошибки";
+            this.label_Action.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 250);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Куда";
+            // 
+            // textBox_From1
+            // 
+            this.textBox_From1.Location = new System.Drawing.Point(31, 24);
+            this.textBox_From1.Name = "textBox_From1";
+            this.textBox_From1.Size = new System.Drawing.Size(314, 20);
+            this.textBox_From1.TabIndex = 4;
+            this.textBox_From1.Text = "\\\\10.39.4.243\\ecall";
+            // 
+            // textBox_To
+            // 
+            this.textBox_To.Location = new System.Drawing.Point(28, 266);
+            this.textBox_To.Name = "textBox_To";
+            this.textBox_To.Size = new System.Drawing.Size(304, 20);
+            this.textBox_To.TabIndex = 5;
+            this.textBox_To.Text = "\\\\10.39.4.1\\ecall";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "От куда";
+            // 
+            // textBox_From2
+            // 
+            this.textBox_From2.Location = new System.Drawing.Point(31, 88);
+            this.textBox_From2.Name = "textBox_From2";
+            this.textBox_From2.Size = new System.Drawing.Size(314, 20);
+            this.textBox_From2.TabIndex = 7;
+            this.textBox_From2.Text = "\\\\10.39.4.246\\ecall";
+            // 
+            // textBox_From3
+            // 
+            this.textBox_From3.Location = new System.Drawing.Point(31, 155);
+            this.textBox_From3.Name = "textBox_From3";
+            this.textBox_From3.Size = new System.Drawing.Size(314, 20);
+            this.textBox_From3.TabIndex = 8;
+            this.textBox_From3.Text = "\\\\10.39.4.247\\ecall";
+            // 
+            // button_About
+            // 
+            this.button_About.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_About.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.button_About.Location = new System.Drawing.Point(311, 389);
+            this.button_About.Name = "button_About";
+            this.button_About.Size = new System.Drawing.Size(21, 23);
+            this.button_About.TabIndex = 9;
+            this.button_About.Text = "!";
+            this.button_About.UseVisualStyleBackColor = true;
+            this.button_About.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(277, 50);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(55, 20);
+            this.numericUpDown1.TabIndex = 10;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(80, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(191, 23);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Копировать последних дней:";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(80, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(191, 23);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Копировать последних дней:";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(277, 115);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(55, 20);
+            this.numericUpDown2.TabIndex = 13;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(277, 181);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(55, 20);
+            this.numericUpDown3.TabIndex = 15;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(80, 178);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(191, 23);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Копировать последних дней:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(10, 27);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 17);
+            this.checkBox1.TabIndex = 16;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.Location = new System.Drawing.Point(10, 91);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(15, 17);
+            this.checkBox2.TabIndex = 17;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.Location = new System.Drawing.Point(10, 158);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(15, 17);
+            this.checkBox3.TabIndex = 18;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(31, 216);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(206, 13);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Копировать каждые                      минут";
+            // 
+            // numericUpDown_timer
+            // 
+            this.numericUpDown_timer.Location = new System.Drawing.Point(142, 212);
+            this.numericUpDown_timer.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown_timer.Name = "numericUpDown_timer";
+            this.numericUpDown_timer.Size = new System.Drawing.Size(55, 20);
+            this.numericUpDown_timer.TabIndex = 21;
+            this.numericUpDown_timer.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(357, 440);
+            this.Controls.Add(this.numericUpDown_timer);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox_From1);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button_About);
+            this.Controls.Add(this.textBox_From3);
+            this.Controls.Add(this.textBox_From2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_To);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label_Action);
+            this.Controls.Add(this.button_Start);
+            this.Controls.Add(this.button_Stop);
+            this.Controls.Add(this.label7);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Копи - СТЕНД вер. 3";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_timer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Stop;
+        private System.Windows.Forms.Button button_Start;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label_Action;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_From1;
+        private System.Windows.Forms.TextBox textBox_To;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_From2;
+        private System.Windows.Forms.TextBox textBox_From3;
+        private System.Windows.Forms.Button button_About;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown numericUpDown_timer;
+    }
+}
+
---
+++ WindowsFormsApplication6/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            button_Stop.Enabled = false;
+            textBox_From1.Enabled = true;
+            numericUpDown1.Enabled = true;
+            textBox_From2.Enabled = false;
+            numericUpDown2.Enabled = false;
+            textBox_From3.Enabled = false;
+            numericUpDown3.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer1.Interval = Convert.ToInt32(numericUpDown_timer.Value * 1000 * 60);
+            if (textBox_From1.Enabled == true)
+            {
+                Copy(textBox_From1.Text, textBox_To.Text, numericUpDown1);
+            }
+            if (textBox_From2.Enabled == true)
+            {
+                Copy(textBox_From2.Text, textBox_To.Text, numericUpDown2);
+            }
+            if (textBox_From3.Enabled == true)
+            {
+                Copy(textBox_From3.Text, textBox_To.Text, numericUpDown3);
+            }
+        }
+        public void Copy(string Dir_from, string Dir_to, NumericUpDown numDay)
+        {
+            try
+            {
+                DirectoryInfo dir_inf = new DirectoryInfo(Dir_from);
+                foreach (DirectoryInfo dir in dir_inf.GetDirectories())
+                {
+                    string folder_from = Dir_from + "\\" + dir.Name;
+                    string folder_to = Dir_to + "\\" + dir.Name;
+                    if (Directory.Exists(Dir_to + "\\" + dir.Name) != true)
+                    {
+                        Directory.CreateDirectory(Dir_to + "\\" + dir.Name);
+                    }
+                    Copy_Dir(folder_from, folder_to, numDay);
+                }
+            }
+            catch (Exception ex)
+            {
+                label_Action.Text = (ex.Message);
+            }
+        }
+
+        public void Copy_Dir(string folder_from, string folder_to, NumericUpDown numDay)
+        {
+            try
+            {
+                DirectoryInfo di = new DirectoryInfo(folder_from);
+                DirectoryInfo[] x = di.GetDirectories();
+                foreach (DirectoryInfo dir_2 in x)
+                {
+                    int c = x.Length;
+                    int a = Convert.ToInt16(dir_2.Name);
+                    if (a >= c)  //if (a >=c-2)
+                    {
+                        Directory.CreateDirectory(folder_to + "\\" + a.ToString().PadLeft(2, '0'));
+                        DirectoryInfo dr = new DirectoryInfo(folder_from + "\\" + a.ToString().PadLeft(2, '0') + "\\");
+                        foreach (FileInfo fi in dr.GetFiles("*.csv"))
+                        {
+                            for (int val = 0; val < numDay.Value; val++)
+                            {
+                                DateTime tt = DateTime.Today.AddDays(-val);
+                                if (fi.CreationTime.Date == tt)
+                                {
+                                    fi.CopyTo(folder_to + "\\" + a.ToString().PadLeft(2, '0') + "\\" + fi.Name, true);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                label_Action.Text = (ex.Message);
+            }
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled == false)
+            {
+                if (textBox_From1.Enabled == true)
+                {
+                    Copy(textBox_From1.Text, textBox_To.Text, numericUpDown1);
+                }
+                if (textBox_From2.Enabled == true)
+                {
+                    Copy(textBox_From2.Text, textBox_To.Text, numericUpDown2);
+                }
+                if (textBox_From3.Enabled == true)
+                {
+                    Copy(textBox_From3.Text, textBox_To.Text, numericUpDown3);
+                }
+            }
+            timer1.Start();
+
+            label_Action.Text = "Копирование файлов запущено!";
+            button_Stop.Enabled = true;
+            button_Start.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            label_Action.Text = "Копирование файлов остановленно.";
+            button_Stop.Enabled = false;
+            button_Start.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Программа создана сотрудником IT отдела для отдела разработки ПО,\n при поддержке программиста Владимира =) !");
+        }
+        
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == false)
+            {
+                textBox_From1.Enabled = false;
+                numericUpDown1.Enabled = false;
+            }
+            else
+            {
+                textBox_From1.Enabled = true;
+                numericUpDown1.Enabled = true;
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked == false)
+            {
+                textBox_From2.Enabled = false;
+                numericUpDown2.Enabled = false;
+            }
+            else
+            {
+                textBox_From2.Enabled = true;
+                numericUpDown2.Enabled = true;
+            }
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked == false)
+            {
+                textBox_From3.Enabled = false;
+                numericUpDown3.Enabled = false;
+            }
+            else
+            {
+                textBox_From3.Enabled = true;
+                numericUpDown3.Enabled = true;
+            }
+        }        
+    }
+}
+
